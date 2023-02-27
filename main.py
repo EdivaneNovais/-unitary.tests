ca8ad59fd104765bb7d22075c9a9b9c88e2db59e
EdivaneNovais <edivane.novais@gmail.com>
2023-02-27 17:58:11 -0300

    cria o arquio pytest.ini, faz o uso dos amarcadores markers e do pytest-cov

---
+++ main.py
@@ -0,0 +1,5 @@
+from bytebank import Funcionario
+
+ana = Funcionario('Ana', '12/03/1999', 1000000)
+
+print(ana.calcular_bonus())
--- tests/test_bytebank.py
+++ tests/test_bytebank.py
@@ -1,4 +1,6 @@
 from codigo.bytebank import Funcionario
+import pytest
+from pytest import mark
 
 class TestClass:
     def test_quando_idade_recebe_13_03_2000_deve_retornar_23(self):
@@ -14,21 +16,57 @@ class TestClass:
         assert resultado == esperado
         
     def test_quando_sobrenome_recebe_Lucas_Carvalho_deve_retornar_Carvalho(self):
-        entrada = ' Lucas Carvalho ' #Given
+        entrada = 'Lucas Carvalho ' #Given
         esperado = 'Carvalho'
         
         lucas = Funcionario(entrada, '11/11/2000', 1111)
         resultado = lucas.sobrenome()#when
         
         assert resultado == esperado #then
-        
+    
     def test_quando_decrescimo_salario_recebe_100000_deve_retornar_90000(self):
-        entrada = 100000 # given
+        entrada_salario = 100000 # given
+        entrada_nome = 'Paulo Bragança'
         esperado = 90000
         
-        funcionario_teste = Funcionario('Teste', '11/11/2000', entrada)
+        funcionario_teste = Funcionario(entrada_nome, '11/11/2000', entrada_salario)
         
         funcionario_teste.decrescimo_salario()# when
         resultado = funcionario_teste.salario
+        
+        assert resultado == esperado # then
+    
+    @mark.calcular_bonus       
+    def test_quando_calcular_bonus_recebe_1000_deve_retornar_100(self):
+        entrada = 1000 # given
+        esperado = 100
+        
+        funcionario_teste = Funcionario('teste', '11/11/2000', entrada)
+        
+        # when
+        resultado = funcionario_teste.calcular_bonus()
+        
+        assert resultado == esperado # then
+        
+    @mark.calcular_bonus    
+    def test_quando_calcular_bonus_recebe_100000000_deve_retornar_exception(self):
+        with pytest.raises(Exception):
+            entrada = 100000000 # given
+        
+            funcionario_teste = Funcionario('teste', '11/11/2000', entrada)
+            
+            # when
+            resultado = funcionario_teste.calcular_bonus()
+            
+            assert resultado  # then
+            
+    def test_retorno_str(self):
+        nome, data_nascimento, salario = 'Teste', '11/11/2000', 1000 # given
+        esperado = 'Funcionario(Teste, 11/11/2000, 1000)'
+        
+        funcionario_teste = Funcionario(nome, data_nascimento, salario)
+        
+        # when
+        resultado = funcionario_teste.__str__()
         
         assert resultado == esperado # then
